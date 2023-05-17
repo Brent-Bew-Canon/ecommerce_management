4e8f3f16ddfacf822d8805903d206a963c2082e7
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-05-17 11:58:19 -0700

    Included Table Associations

--- models/index.js
+++ models/index.js
@@ -4,13 +4,30 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// DONE: Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
-// Categories have many Products
+// DONE: Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
-// Products belongToMany Tags (through ProductTag)
+// DONE: Products belongToMany Tags (through ProductTag)
+// * Check that this syntax actually works
+Product.belongsToMany(Tag, {
+  through: ProductTag
+})
 
-// Tags belongToMany Products (through ProductTag)
+
+// DONE: Tags belongToMany Products (through ProductTag)
+// * Check that this syntax actually works
+Tag.belongsToMany(Product, {
+  through: ProductTag
+})
+
 
 module.exports = {
   Product,
